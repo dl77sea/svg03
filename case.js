1f6b07dac56a3a231f05e5198ec530bc794c1070
dl77sea <danlarsson77@gmail.com>
2018-01-18 23:42:38 -0800

    added some form elements

--- case.js
+++ case.js
@@ -4,8 +4,8 @@ class Stick {
   constructor(strOrientation) {
     this.vrt = false;
     this.hrz = false;
-    if(strOrientation === "hrz") this.hrz = true
-    if(strOrientation === "vrt") this.vrt = true
+    if (strOrientation === "hrz") this.hrz = true
+    if (strOrientation === "vrt") this.vrt = true
   }
 }
 
@@ -27,14 +27,14 @@ class Node {
     this.lowerRightY = faceLrY
 
     //touch face
-    this.width  = this.lowerRightX - faceUlX
+    this.width = this.lowerRightX - faceUlX
     this.height = this.upperLeftY - faceLrY
 
     //stick (this is where info about the stick is contained if node has stick)
     this.hrz = false
     this.vrt = false
-    if(stick.hrz) this.hrz = true
-    if(stick.vrt) this.vrt = true
+    if (stick.hrz) this.hrz = true
+    if (stick.vrt) this.vrt = true
 
     //children (left and right or top and bottom partitions)
     this.children = []
@@ -49,7 +49,7 @@ function partition(clickedFaceNode, faceEventX, faceEventY, orientation) {
 
   //add children and stick
 
-  if(orientation === "hrz") {
+  if (orientation === "hrz") {
     //hrz
 
     //add face id (used for associating stick with face, when stick is selected)
@@ -104,37 +104,45 @@ function partition(clickedFaceNode, faceEventX, faceEventY, orientation) {
 function partitionEquals(clickedFaceNode, divisions, orientation) {
 
   //get partition width and height for equal divisions
-  let partitionWidth = clickedFaceNode.width/divisions
-  let partitionHeight = clickedFaceNode.height/divisions
+  let partitionWidth = clickedFaceNode.width / divisions
+  let partitionHeight = clickedFaceNode.height / divisions
 
-  if(orientation === "hrz") {
+  if (orientation === "hrz") {
     //hrz
-    for(let i=0; i < divisions; i++) {
+    for (let i = 0; i < divisions; i++) {
       idTicker++
-      if(i === divisions-1) {stick = stickNone} else {stick = stickHrz}
+      if (i === divisions - 1) {
+        stick = stickNone
+      } else {
+        stick = stickHrz
+      }
       clickedFaceNode.children.push(new Node(
         //submit upper-left face point
         clickedFaceNode.upperLeftX,
-        clickedFaceNode.lowerRightY + partitionHeight*(i+1),
+        clickedFaceNode.lowerRightY + partitionHeight * (i + 1),
         //submit lower-right face point
         clickedFaceNode.lowerRightX,
-        clickedFaceNode.lowerRightY + partitionHeight*i,
+        clickedFaceNode.lowerRightY + partitionHeight * i,
         idTicker,
         stick))
 
     }
   }
 
-  if(orientation === "vrt") {
-    for(let i=0; i < divisions; i++) {
+  if (orientation === "vrt") {
+    for (let i = 0; i < divisions; i++) {
       idTicker++
-      if(i === 0) {stick = stickNone} else {stick = stickVrt}
+      if (i === 0) {
+        stick = stickNone
+      } else {
+        stick = stickVrt
+      }
       clickedFaceNode.children.push(new Node(
         //submit upper-left face point
-        clickedFaceNode.upperLeftX + partitionWidth*i,
+        clickedFaceNode.upperLeftX + partitionWidth * i,
         clickedFaceNode.upperLeftY,
         //submit lower-right face point
-        clickedFaceNode.upperLeftX + partitionWidth*(i+1),
+        clickedFaceNode.upperLeftX + partitionWidth * (i + 1),
         clickedFaceNode.lowerRightY,
         idTicker,
         stick))
@@ -142,14 +150,7 @@ function partitionEquals(clickedFaceNode, divisions, orientation) {
   }
 }
 
-//faceUlX, faceUlY, faceLrX, faceLrY, strData
-var rootWidth = 100
-var rootHeight = 100
-var rootNode = new Node(  (rootWidth/2)*(-1),rootHeight,
-                          (rootWidth/2),0,
-                          0, stickNone)
-
-const nodeA = new Node(0,10,10,0,0, stickNone)
+// const nodeA = new Node(0,10,10,0,0, stickNone)
 
 //ui modes
 let orientation = null
@@ -157,15 +158,15 @@ let orientation = null
 function traverseTree(fn) {
   let nodes = [rootNode]
 
-  while(nodes.length > 0) {
+  while (nodes.length > 0) {
     let node = nodes.pop()
 
     // //handle node 0
     // if(node)
     // fn(node)
 
-    if(node.children.length > 0) {
-      for(node of node.children) {
+    if (node.children.length > 0) {
+      for (node of node.children) {
         fn(node)
         nodes.push(node)
       }
@@ -175,7 +176,9 @@ function traverseTree(fn) {
 
 function findNode(faceId) {
   let n = null
-  traverseTree(function(node){if(faceId === node.faceId) n = node})
+  traverseTree(function(node) {
+    if (faceId === node.faceId) n = node
+  })
   return n
 }
 
@@ -184,8 +187,8 @@ function snarf(node) {
 }
 
 function createLine(x1, y1, x2, y2, id) {
-  let line = document.createElementNS('http://www.w3.org/2000/svg','line')
-  line.setAttribute('id', "seg"+id)
+  let line = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+  line.setAttribute('id', "seg" + id)
   line.setAttribute('class', 'lineseg')
   line.setAttribute('x1', x1)
   line.setAttribute('y1', y1)
@@ -194,7 +197,51 @@ function createLine(x1, y1, x2, y2, id) {
   return line
 }
 
-function plotCase() {
+
+
+function plotPartition(node) {
+
+  if (node.hrz) {
+    //on upperLeftY
+    let line = createLine(node.upperLeftX, node.upperLeftY, node.lowerRightX, node.upperLeftY, node.faceId)
+    svgEl.append(line)
+  }
+  if (node.vrt) {
+    console.log(node)
+    // on lowerRightX
+    // let line=createLine(node.lowerRightX, node.upperLeftY, node.lowerRightX, node.lowerRightY, node.faceId)
+    let line = createLine(node.upperLeftX, node.upperLeftY, node.upperLeftX, node.lowerRightY, node.faceId)
+    svgEl.append(line)
+  }
+}
+
+
+var rootNode
+
+function plotCase(w, h) {
+  let rootWidth = w
+  let rootHeight = h
+
+  idTicker = 0;
+
+  while (svgEl.firstChild) {
+    console.log("....", svgEl.firstChild)
+    svgEl.removeChild(svgEl.firstChild);
+  }
+
+  rootNode = new Node(
+    (rootWidth / 2) * (-1), rootHeight,
+    (rootWidth / 2), 0,
+    0,
+    stickNone)
+
+  // partition(rootNode, 3, 75, "hrz")
+  // partitionEquals(findNode(2), 3, "vrt")
+  // partition(findNode(4), 3, 38, "hrz")
+  // partitionEquals(findNode(5), 3, "hrz")
+  // partition(findNode(1), 10, 3, "vrt")
+  // partitionEquals(findNode(7), 3, "vrt")
+
   let lineTop = createLine(rootNode.upperLeftX, rootNode.upperLeftY, rootNode.lowerRightX, rootNode.upperLeftY, rootNode.faceId)
   let lineRight = createLine(rootNode.lowerRightX, rootNode.upperLeftY, rootNode.lowerRightX, rootNode.lowerRightY, rootNode.faceId)
   let lineBottom = createLine(rootNode.lowerRightX, rootNode.lowerRightY, rootNode.upperLeftX, rootNode.lowerRightY, rootNode.faceId)
@@ -207,32 +254,21 @@ function plotCase() {
   traverseTree(plotPartition)
 }
 
-function plotPartition(node) {
+document.getElementById('form-overall').addEventListener('submit', (event) => {
+  event.preventDefault();
+  plotCase(event.target.width.value, event.target.height.value)
+})
 
-  if(node.hrz) {
-    //on upperLeftY
-    let line=createLine(node.upperLeftX, node.upperLeftY, node.lowerRightX, node.upperLeftY, node.faceId)
-    svgEl.append(line)
-  }
-  if(node.vrt) {
-    console.log(node)
-    // on lowerRightX
-    // let line=createLine(node.lowerRightX, node.upperLeftY, node.lowerRightX, node.lowerRightY, node.faceId)
-    let line=createLine(node.upperLeftX, node.upperLeftY, node.upperLeftX, node.lowerRightY, node.faceId)
-    svgEl.append(line)
+let partitionTypes = document.getElementsByName('partition-type')
+for(pt of partitionTypes) {
+  if(pt.checked === true) {
+    console.log(pt.value)
   }
 }
 
+
 //build a tree
 // orientation: "hrz" "vrt"
-partition(rootNode, 3, 75, "hrz")
-partitionEquals(findNode(2), 3, "vrt")
-partition(findNode(4), 3, 38, "hrz")
-partitionEquals(findNode(5), 3, "hrz")
-partition(findNode(1), 10, 3, "vrt")
-partitionEquals(findNode(7), 3, "vrt")
-console.log(findNode(1))
-// partition(traverseTree(5, snarf),   2.5,1, "hrz")
-plotCase()
+
 // console.log("---")
 // traverseTree(7, snarf)
